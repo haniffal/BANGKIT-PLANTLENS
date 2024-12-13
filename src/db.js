69e90381e657840719fb9e1f68d6d1657f8d7715
Bagas Rosyidi <bagasrosyidi@gmail.com>
2024-12-13 14:06:30 +0700

    Update db.js

--- src/db.js
+++ src/db.js
@@ -1,15 +1,15 @@
 const mysql = require('mysql2');
 
-// Membuat koneksi ke MySQL
+
 const connection = mysql.createConnection({
-    host: '',        // Ganti dengan IP instance MySQL Anda
-    user: 'root',                  // Ganti dengan username MySQL Anda
-    database: '',       // Ganti dengan nama database Anda
-    password: ''        // Ganti dengan password MySQL Anda
+    host: '',            // Ganti dengan IP instance MySQL Anda
+    user: 'root',        // Ganti dengan username MySQL Anda
+    database: '',        // Ganti dengan nama database Anda
+    password: ''         // Ganti dengan password MySQL Anda
     
 });
 
-// Mengecek koneksi
+
 connection.connect((err) => {
     if (err) {
         console.error('Koneksi gagal:', err.message);
